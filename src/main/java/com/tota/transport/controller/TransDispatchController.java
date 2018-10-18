c2c0cb0132ee369c3da69cd153244e21ec057f19
982941440 <982941440@qq.com>
2018-10-18 10:01:41 +0800

    订单回调

--- src/main/java/com/tota/transport/controller/TransDispatchController.java
+++ src/main/java/com/tota/transport/controller/TransDispatchController.java
@@ -3,10 +3,13 @@ package com.tota.transport.controller;
 
 import com.tota.se.common.domain.Result;
 import com.tota.se.common.domain.ResultCode;
+import com.tota.se.common.util.date.DateUtil;
 import com.tota.transport.enums.TransTypeEnum;
 import com.tota.transport.platform.ITransportBaseService;
+import com.tota.transport.platform.dada.model.req.body.Callback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -30,6 +33,12 @@ public class TransDispatchController {
         return "welcome";
     }
 
+    @RequestMapping("/toCallback")
+    public String toCallback(HttpServletRequest req) {
+
+        return "callback";
+    }
+
     @RequestMapping("/transport")
     @ResponseBody
     public Result transport(HttpServletRequest req) {
@@ -47,6 +56,20 @@ public class TransDispatchController {
             e.printStackTrace();
         }
         return new Result(ResultCode.COMMON_SYSTEM_ERROR, false);
+    }
+
+
+    @RequestMapping("/callback")
+    @ResponseBody
+    public Result callback(@RequestBody Callback callback) {
+
+        Result result = new Result(ResultCode.COMMON_SUCCESS, true);
+        result.setUseDateFormat(true);
+        result.setDateFormat(DateUtil.LONG_DATE_FORMAT_STR);
+
+        result.setProperty("resultMap",callback);
+
+        return result;
     }
 
 }
